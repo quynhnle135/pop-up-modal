a275e88d39e5db31a0cffe712677b8b094f25f22
Quinn Le <quynhnle135@gmail.com>
2024-02-28 19:30:58 -0500

    Implemented pop-up

---
+++ script.js
@@ -0,0 +1,11 @@
+let open = document.getElementById("open");
+let close = document.getElementById("close");
+let modal_container = document.getElementById("modal_container");
+
+open.addEventListener("click", () => {
+    modal_container.classList.add("show");
+})
+
+close.addEventListener("click", () => {
+    modal_container.classList.remove("show");
+})
